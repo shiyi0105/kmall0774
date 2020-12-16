02e713dfefd71a99ebe6ba711149ee616d493db8
shiyi_glb <1135537585@qq.com>
2020-12-16 19:29:06 +0800

    创建kmall-manager

--- kmall-manager-service/src/main/java/com/kgc/kmall/manager/KmallManagerServiceApplication.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/KmallManagerServiceApplication.java
@@ -1,8 +1,11 @@
 package com.kgc.kmall.manager;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableDubbo
+@MapperScan(basePackages = "com.kgc.kmall.manager.mapper")
 @SpringBootApplication
 public class KmallManagerServiceApplication {
 
