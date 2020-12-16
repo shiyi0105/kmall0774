58782c0836de4b10b3e539331cfb99531d2822bf
shiyi_glb <1135537585@qq.com>
2020-12-16 19:24:57 +0800

    创建kmall-manager

---
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/KmallManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kgc.kmall.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmallManagerServiceApplication.class, args);
+	}
+
+}
