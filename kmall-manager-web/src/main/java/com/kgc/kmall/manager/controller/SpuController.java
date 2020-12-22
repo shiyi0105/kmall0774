18b04fb1722becb9b217d70b5a4728952e370f39
shiyi_glb <1135537585@qq.com>
2020-12-22 11:09:51 +0800

    创建kmall-manager

--- kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/SpuController.java
@@ -3,10 +3,18 @@ package com.kgc.kmall.manager.controller;
 import com.kgc.kmall.bean.PmsBaseSaleAttr;
 import com.kgc.kmall.bean.PmsProductInfo;
 import com.kgc.kmall.service.SpuService;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.dubbo.config.annotation.Reference;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,6 +26,9 @@ public class SpuController {
     @Reference
     SpuService spuService;
 
+    @Value("{fileServer.url}")
+    String fileServer;
+
     @RequestMapping("/spuList")
     public List<PmsProductInfo> spuList(Long catalog3Id){
         List<PmsProductInfo> infoList = spuService.spuList(catalog3Id);
@@ -28,6 +39,33 @@ public class SpuController {
     public List<PmsBaseSaleAttr> baseSaleAttrList(){
         List<PmsBaseSaleAttr> saleAttrList = spuService.baseSaleAttrList();
         return saleAttrList;
+    }
+    @RequestMapping("")
+    public  String fileUpload(@RequestParam("file")MultipartFile file){
+        try{
+            String confFile = this.getClass().getResource("/tracker.conf").getFile();
+            ClientGlobal.init(confFile);
+            TrackerClient trackerClient=new TrackerClient();
+            TrackerServer trackerServer=trackerClient.getTrackerServer();
+            StorageClient storageClient=new StorageClient(trackerServer,null);
+            String orginalFilename=file.getOriginalFilename();
+            String extName = FilenameUtils.getExtension(orginalFilename);
+            String[] upload_file = storageClient.upload_file(file.getBytes(), extName, null);
+            String path=fileServer;
+            for (int i = 0; i < upload_file.length; i++) {
+                String s = upload_file[i];
+                System.out.println("s = " + s);
+                path+="/"+s;
+            }
+            System.out.println(path);
+            return path;
+        }catch (Exception e){
+            e.printStackTrace();
+            return  null;
+        }
+
+
+
     }
 
     @RequestMapping("/saveSpuInfo")
--- kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
+++ kmall-manager-web/src/test/java/com/kgc/kmall/manager/KmallManagerWebApplicationTests.java
@@ -1,5 +1,9 @@
 package com.kgc.kmall.manager;
 
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +11,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 class KmallManagerWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()  {
+		try{
+			String file = this.getClass().getResource("/tracker.conf").getFile();
+			ClientGlobal.init(file);
+			TrackerClient trackerClient=new TrackerClient();
+			TrackerServer trackerServer=trackerClient.getTrackerServer();
+			StorageClient storageClient=new StorageClient(trackerServer,null);
+			String orginalFilename="C:\\Users\\machenike\\Desktop\\壁纸\\11.jpg";
+			String[] upload_file = storageClient.upload_file(orginalFilename, "jpg", null);
+			String path="http://192.168.136.130";
+			for (int i = 0; i < upload_file.length; i++) {
+				String s = upload_file[i];
+				System.out.println("s = " + s);
+				path+="/"+s;
+			}
+			System.out.println(path);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
